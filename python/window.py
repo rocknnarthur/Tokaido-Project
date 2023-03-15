ecfedcfe6d25f1dc624dce23fc2f8114fe296a6e
Art333 <rocknnarthur@gmail.com>
2023-03-15 18:17:50 +0100

    modif

--- python/window.py
+++ python/window.py
@@ -11,7 +11,7 @@ class Game:
         self.overworld.run()
 
 pygame.init()
-pygame.display.set_caption("Tokaido")
+pygame.display.set_caption("Tokaido", "../icon_game.png")
 screen = pygame.display.set_mode((screen_width,screen_height))
 clock = pygame.time.Clock()
 game = Game()
