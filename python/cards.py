747807724302dbf654fa7ad336287fd811cc9308
Art333 <rocknnarthur@gmail.com>
2023-03-23 15:16:35 +0100

    merge

--- python/cards.py
+++ python/cards.py
@@ -4,12 +4,17 @@ class Souvenir:
         self.category = famille
         self.price = prix
     
-class Panorama:
+class PanoramaCard:
     def __init__(self, nom, paysage, nombre):
         self.name = nom
         self.land = paysage
         self.nbr = nombre
 
-class SourceChaude:
+class SourceChaudeCard:
     def __init__(self, points):
         self.pts = points
+
+class RencontreCard:
+    def __init__(self, nom, effet):
+        self.name = nom
+        self.effect = effet
--- python/sqlconnexion.py
+++ python/sqlconnexion.py
@@ -12,20 +12,14 @@ cursor = cnx.cursor()
 
 today = datetime.now().date()
 
+# Query
 add_joueur = ("INSERT INTO Joueur "
                "(jou_id, jou_nom, jou_prenom, jou_pseudo, jou_creation, jou_login, jou_password) "
                "VALUES (%s, %s, %s, %s, %s, %s, %s)")
 
 data_joueur = (2, "AMISI", "Arthur", "Vitarse", today, "vitarse", "azerty123*")
 
-row = """\nINSERT INTO Joueur (jou_id, jou_nom, jou_prenom, jou_pseudo, jou_creation, jou_login, jou_password)
-VALUES (2, "AMISI", "Arthur", "Vitarse", today, "vitarse", "azerty123*");"""
-
-fic = open("../sql/tokaido.sql", "a")
-fic.write(row)
-fic.close
-
-# Insert new employee
+# Execute query
 cursor.execute(add_joueur, data_joueur)
 
 # Make sure data is committed to the database
